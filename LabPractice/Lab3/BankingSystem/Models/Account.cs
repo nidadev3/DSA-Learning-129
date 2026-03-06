4a9dafc1eccee3903e37688533ed3ef88deb0ef2
Nida Arshad <nidaarshad120@gmail.com>
2026-03-06 03:44:00 -0800

    Set the abstract account class

--- LabPractice/Lab3/BankingSystem/Models/Account.cs
+++ LabPractice/Lab3/BankingSystem/Models/Account.cs
@@ -7,13 +7,60 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Models
 {
- public abstract class Account
+    abstract class Account
     {
-        protected int accountId { get; set; }
-        protected string name { get; set; }
-        protected uint permissions { get; set; }
-        protected double[] transactions { get; set; }
-        protected int transactionCount { get; set; }
+        protected int accountId;
+        protected string name;
+        protected double balance;
+        protected uint permissions;
+        protected double[] transactions;
+        protected int transactionCount;
 
+        protected Account(int accountId, string name, uint permissions)
+        {
+            this.accountId = accountId;
+            this.name = name;
+            this.permissions = permissions;
+            this.transactions = new double[100];
+            this.transactionCount = 0;
+        }
+
+        public abstract void Deposit(double amount);
+        public abstract void Withdraw(double amount);
+        public abstract void SaveToFile();
+        public abstract void LoadFromFile();
+
+        protected void AddTransaction(double amount)
+        {
+            if (transactionCount < transactions.Length)
+            {
+                transactions[transactionCount++] = amount;
+            }
+        }
+
+        public void ShowTransactions()
+        {
+            for (int i = 0; i < transactionCount; i++)
+            {
+                Console.WriteLine(transactions[i]);
+            }
+        }
+
+        protected bool CanWithdraw()
+        {
+            return (permissions & 1) != 0;
+        }
+
+        protected bool CanDeposit()
+        {
+            return (permissions & 2) != 0;
+        }
+        public void ShowAccountInfo()
+        {
+            Console.WriteLine("Account ID: " + accountId);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Balance: " + balance);
+            Console.WriteLine("VIP: " + ((permissions & 8) != 0));
+        }
     }
 }
