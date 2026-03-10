3725105c53f457aedd6738ec025df3069e6bd0da
Nida Arshad <nidaarshad120@gmail.com>
2026-03-10 09:09:15 -0700

    Traversing through array

--- mypractice/Arrays.cpp
+++ mypractice/Arrays.cpp
@@ -38,3 +38,18 @@ using namespace std;
 //     }
 //     cout<<"Largest element in the array: "<<Largest<<endl;
 // }
+//Printing tha data in the array
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int* arr=new int[n];
+    cout<<"Enter "<<n<<" elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Array elements: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
