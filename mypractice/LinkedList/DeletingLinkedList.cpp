a5a4c480efc6ccae5f3029b0ecaf73aa3dbe0ba9
Nida Arshad <nidaarshad120@gmail.com>
2026-03-11 19:18:08 -0700

    Deletion of Linked List

--- mypractice/LinkedList/DeletingLinkedList.cpp
+++ mypractice/LinkedList/DeletingLinkedList.cpp
@@ -4,6 +4,42 @@ struct Node{
     int data;
     Node *link;
 };
-Node * DeleteAtanyPosition(Node *head,int position){
-    
+void deletelist(Node *&head){
+    Node *temp=head;
+    while(temp!=nullptr){
+        Node *nextnode=temp->link;
+        delete temp;
+        temp=nextnode;
+
+    }
+    head=nullptr;
+}
+int main (){
+    Node* head=nullptr;
+    // Creating a linked list with some nodes
+    head=new Node();
+    head->data=10;
+    head->link=new Node();
+    head->link->data=20;
+    head->link->link=new Node();
+    head->link->link->data=30;
+    head->link->link->link=nullptr;
+
+    cout<<"Original List: ";
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->link;
+    }
+    cout<<endl;
+
+   deletelist(head);
+
+    cout<<"List after deleting all nodes: ";
+    temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->link;
+    }
+     cout<<endl;
 }
