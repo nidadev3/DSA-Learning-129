95ca1eba7761474aed10b10d5d295fe46e32951f
Nida Arshad <nidaarshad120@gmail.com>
2026-03-12 22:56:36 -0700

    The min element in the array

---
+++ LabPractice/Lab4/ArraysOperation/mininArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int arr[100];
+    int n;
+    cout<<"Enter the size of array :";
+    cin>>n;
+    cout<<"Enter the elements of the array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int smallest=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<smallest){
+            smallest=arr[i];
+        }
+      
+    }
+      cout<<"The smallest element in array is:"<<smallest;
+}
