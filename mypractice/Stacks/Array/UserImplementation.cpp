1885a67a089813a415e342b2516de63b70bc31d1
Nida Arshad <nidaarshad120@gmail.com>
2026-03-19 23:05:53 -0700

    Completed Stack Impelementation using stack

--- mypractice/Stacks/Array/UserImplementation.cpp
+++ mypractice/Stacks/Array/UserImplementation.cpp
@@ -1,49 +1,112 @@
 #include<iostream>
 using namespace std;
 
-class stack{
-    private:
+class stack {
+private:
     int* arr;
     int capacity;
     int top;
-    public:
-    stack(int capacity){
-        this.capcacity=capacity;
-        arr=new int[capacity];
-        top=-1
-    }
-    //isFull function
-    bool isFull(){
-if(top==capcity-1){
-    cout<<"Stack is overflow"<<endl;
-    return 1;
-}
-else{
-    return 0;
-}
 
+public:
+    stack(int capacity) {
+        this->capacity = capacity;
+        arr = new int[capacity];
+        top = -1;
     }
-    //isEmpty Function
-    bool isEmpty(){
-        if (top==-1){
-            cout<<"Stack is empty"<<endl;
-            return 1;
+
+    bool isFull() {
+        if (top == capacity - 1) {
+            cout << "Stack is overflow" << endl;
+            return true;
         }
-        else{
-            return 0;
+        return false;
+    }
+
+    bool isEmpty() {
+        if (top == -1) {
+            cout << "Stack is empty" << endl;
+            return true;
+        }
+        return false;
+    }
+
+    void push(int value) {
+        if (isFull()) return;
+        arr[++top] = value;
+    }
+
+    int pop() {
+        if (isEmpty()) return -1;
+        return arr[top--];
+    }
+
+    int peek() {
+        if (isEmpty()) return -1;
+        return arr[top];
+    }
+
+    void display() {
+        if (isEmpty()) return;
+
+        cout << "Stack elements:\n";
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << endl;
         }
     }
-    
-}
-int main (){
-    int choice, int data;
-    while(1){
-        cout<<"1-Perform push operation";
-        cout<<"2-Perform pop operation";
-        cout<<"3-Print the element at the top";
-        cout<<"4-Print all elements in the stack";
-        cout<<"5-Exit";
-        cout<<"Enter you choice:";
-        cin>>choice;
+};
+
+int main() {
+    int choice, data;
+
+    cout << "Enter the capacity of stack: ";
+    cin >> data;
+
+    stack s(data);
+
+    while (1) {
+        cout << "\n1- Push\n";
+        cout << "2- Pop\n";
+        cout << "3- Peek (Top)\n";
+        cout << "4- Display\n";
+        cout << "5- Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            int n;
+            cout << "How many elements you want to push? ";
+            cin >> n;
+
+            for (int i = 0; i < n; i++) {
+                if (s.isFull()) break;
+
+                int val;
+                cout << "Enter value: ";
+                cin >> val;
+                s.push(val);
+            }
+        }
+        else if (choice == 2) {
+            int val = s.pop();
+            if (val != -1)
+                cout << "Popped: " << val << endl;
+        }
+        else if (choice == 3) {
+            int val = s.peek();
+            if (val != -1)
+                cout << "Top element: " << val << endl;
+        }
+        else if (choice == 4) {
+            s.display();
+        }
+        else if (choice == 5) {
+            cout << "Exiting program...\n";
+            break;
+        }
+        else {
+            cout << "Invalid choice!\n";
+        }
     }
+
+    return 0;
 }
