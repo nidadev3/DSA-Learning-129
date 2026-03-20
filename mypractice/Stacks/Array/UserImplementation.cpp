e07bdf8dcd490f53bef9221a8b41b654308c2ec0
Nida Arshad <nidaarshad120@gmail.com>
2026-03-19 22:33:20 -0700

    Added menu function in the main

---
+++ mypractice/Stacks/Array/UserImplementation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int choice, int data;
+    while(1){
+        cout<<"1-Perform push operation";
+        cout<<"2-Perform pop operation";
+        cout<<"3-Print the element at the top";
+        cout<<"4-Print all elements in the stack";
+        cout<<"5-Exit";
+        cout<<"Enter you choice:";
+        cin>>choice;
+    }
+}
