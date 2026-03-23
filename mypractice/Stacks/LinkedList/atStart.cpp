9b3a3fbf6883de260016797c4641e45f2c685fbb
Nida Arshad <nidaarshad120@gmail.com>
2026-03-23 04:15:44 -0700

    Completed stack implementation using linked list (head)

--- mypractice/Stacks/LinkedList/atStart.cpp
+++ mypractice/Stacks/LinkedList/atStart.cpp
@@ -1,46 +1,95 @@
 #include <iostream>
 using namespace std;
-struct Node{
+
+struct Node {
     int data;
     Node* link;
-    Node(int data){
-        this.data=data;
-        link=nullptr
+
+    Node(int data) {
+        this->data = data;
+        link = nullptr;
     }
 };
-class stack{
-    private:
+
+class Stack {
+private:
     Node* top;
-    public:
-    stack(){
-        top=nullptr;
+
+public:
+    Stack() {
+        top = nullptr;
     }
-    bool isEmpty(){
-        retun top=nullptr;
+
+    bool isEmpty() {
+        return top == nullptr;
     }
-    void push(int x){
-        Node* newnode=new Node(x);
-        newnode->next=top;
-        top=newnode;
-        cout<<x<<"pushed in to the stack";
+
+    void push(int x) {
+        Node* newnode = new Node(x);
+        newnode->link = top;
+        top = newnode;
+
+        cout << x << " pushed into the stack\n";
     }
-    void pop (){
-        if(isEmpty){
-            cout<<"Stack is overflow";
-            return -1;
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow\n";
+            return;
         }
-        Node* temp=top;
-        cout<<top->data<<"is popped";
-        top=top->next;
+
+        Node* temp = top;
+        cout << top->data << " is popped\n";
+
+        top = top->link;
         delete temp;
-
-
     }
-    void peak(){
-          if(isEmpty){
-            cout<<"Stack is overflow";
-            return -1;
+
+    void peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty\n";
+            return;
         }
-      return  cout<<Top->data<<"is the top value";
+
+        cout << top->data << " is the top value\n";
     }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+
+        Node* temp = top;
+        cout << "Stack elements: ";
+
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->link;
+        }
+
+        cout << endl;
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    s.peek();
+
+    s.pop();
+    s.display();
+
+    s.pop();
+    s.pop();
+
+    s.pop(); // underflow test
+
+    return 0;
 }
