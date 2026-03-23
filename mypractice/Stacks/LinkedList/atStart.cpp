b22c1151e0cec2fa20c5a61260008c725072a3b0
Nida Arshad <nidaarshad120@gmail.com>
2026-03-23 03:46:26 -0700

    Declared structure of node

--- mypractice/Stacks/LinkedList/atStart.cpp
+++ mypractice/Stacks/LinkedList/atStart.cpp
@@ -1,49 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node {
-public:
-int data;
-Node* next;
+struct Node{
+    int data;
+    Node* link;
+    Node(int data){
+        this.data=data;
+        link=nullptr
+    }
 };
-class Stack {
-Node* top;
-public:
-Stack() {
-top = NULL;
-}
-void push(int x) {
-Node* newNode = new Node();
-newNode->data = x;
-newNode->next = top;
-top = newNode;
-}
-void pop() {
-if (top == NULL) {
-cout << "Stack Underflow\n";
-return;
-
-}
-Node* temp = top;
-cout << temp->data << " popped\n";
-top = top->next;
-delete temp;
-}
-void display() {
-Node* temp = top;
-while (temp != NULL) {
-cout << temp->data << " ";
-temp = temp->next;
-}
-cout << endl;
-}
-};
-int main() {
-Stack s;
-s.push(5);
-s.push(10);
-s.push(15);
-s.display();
-s.pop();
-s.display();
-return 0;
-}
