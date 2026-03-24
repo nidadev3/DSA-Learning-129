49749e94ef283c47a85fa88b6edaa68e7c7adf05
Nida Arshad <nidaarshad120@gmail.com>
2026-03-24 03:21:03 -0700

    Parenthesis check using stack without stl (array)

--- Assignments/Stack/ParenthesisA.cpp
+++ Assignments/Stack/ParenthesisA.cpp
@@ -1,38 +1,101 @@
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+
+// bool isBalanced(char *s) {
+//     stack<char> st;
+
+//     for(int i = 0; s[i] != '\0'; i++) {
+//         if (s[i] == '[' || s[i] == '{' || s[i] == '(') {
+//             st.push(s[i]);
+//         }
+//         else if (s[i] == ']' || s[i] == '}' || s[i] == ')') {
+//             if (st.empty()) return false;
+
+//             char top = st.top();
+//             st.pop();
+
+//             if ((s[i] == ')' && top != '(') ||
+//                 (s[i] == '}' && top != '{') ||
+//                 (s[i] == ']' && top != '[')) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return st.empty();
+// }
+
+// int main() {
+//     char exp[100];
+//     cout << "Enter Expression: ";
+//     cin.getline(exp, 100);
+
+//     if (isBalanced(exp))
+//         cout << "Balanced";
+//     else
+//         cout << "Not Balanced";
+// }
+
+
+//Without STL
 #include<iostream>
-#include<string>
-#include<stack>
 using namespace std;
-#define max=100;
-char st[max];
 
-bool isBalanced(char *s){
-    for(int i=0;i<s.length;i++){
-        if(
-            s[i]='['|| s[i]='{'|| s[i]='('
-        ){
-            st.push(s);
-        }
-        else if( s[i]=']'|| s[i]='}'|| s[i]=')')
-        {
-if(st.empty()) return false;
-s.top=st.top();
-st.pop();
-    
-         if ((s == ')' && top != '(') ||
-                (s == '}' && top != '{') ||
-                (s == ']' && top != '[')) {
-                return false;
+char stackarr[100];
+int top = -1;
+
+bool isEmpty() {
+    return (top == -1);
+}
+
+void push(char x) {
+    if (top == 99) {
+        cout << "Overflow";
+        return;
     }
+    top++;
+    stackarr[top] = x;
 }
+
+void pop() {
+    if (isEmpty()) {
+        cout << "Underflow";
+        return;
+    }
+    top--;
 }
-return st.empty();}
 
+char peek() {
+    return stackarr[top];
+}
 
-int main (){
+bool isBalanced(char *s) {
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            push(s[i]);
+        }
+        else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+            if (isEmpty()) return false;
+
+            char topchar = peek();
+            pop();
+
+            if ((s[i] == ')' && topchar != '(') ||
+                (s[i] == '}' && topchar != '{') ||
+                (s[i] == ']' && topchar != '[')) {
+                return false;
+            }
+        }
+    }
+    return isEmpty();
+}
+
+int main() {
     char exp[100];
-    cout<<"Enter the Expression:";
-    cin.getline(exp,100);
-     if (isBalanced(expr))
+    cout << "Enter Expression: ";
+    cin.getline(exp, 100);
+
+    if (isBalanced(exp))
         cout << "Balanced";
     else
         cout << "Not Balanced";
