047b613a76ceb927c4ff427cc86fe0d52c9f02bd
Nida Arshad <nidaarshad120@gmail.com>
2026-03-24 16:52:06 -0700

    Main stack functions

--- Assignments/Stack/InfixtoPostfix.cpp
+++ Assignments/Stack/InfixtoPostfix.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
 using namespace std;
 
-#define max 100;
+#define max 100
 char stackarr[max];
 int top=-1;
-
+bool isEmpty(){
+    return(top==-1);
+}
+void push (char x){
+    if(top==99){
+        cout<<"Stack is overflow";
+        return;
+    }
+    top++;
+    stackarr[top]=x;
+}
+char pop(){
+    if(isEmpty()){
+        cout << "Stack underflow";
+        return -1;
+    }
+    char val = stackarr[top];
+    top--;
+    return val;
+}
+char peek(){
+return stackarr[top];
+}
 int main (){
     char exp[100];
     cout<<"Enter the expression:";
