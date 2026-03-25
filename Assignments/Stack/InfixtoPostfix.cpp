4a824d5cf9b6d31f64bf9b9380e126468d7a4286
Nida Arshad <nidaarshad120@gmail.com>
2026-03-24 17:19:34 -0700

    Parenthesis check

--- Assignments/Stack/InfixtoPostfix.cpp
+++ Assignments/Stack/InfixtoPostfix.cpp
@@ -2,19 +2,26 @@
 using namespace std;
 
 #define max 100
+
 char stackarr[max];
-int top=-1;
+int top = -1;
+
+// Check if stack is empty
 bool isEmpty(){
-    return(top==-1);
+    return (top == -1);
 }
-void push (char x){
-    if(top==99){
-        cout<<"Stack is overflow";
+
+// Push function
+void push(char x){
+    if(top == max - 1){
+        cout << "Stack is overflow";
         return;
     }
     top++;
-    stackarr[top]=x;
+    stackarr[top] = x;
 }
+
+// Pop function
 char pop(){
     if(isEmpty()){
         cout << "Stack underflow";
@@ -24,37 +31,60 @@ char pop(){
     top--;
     return val;
 }
+
+// Peek function
 char peek(){
-return stackarr[top];
+    if(top == -1) return '\0';
+    return stackarr[top];
 }
+
+// Precedence function
 int precedence(char ch) {
     if (ch == '+' || ch == '-') return 1;
     if (ch == '*' || ch == '/') return 2;
     return 0;
 }
 
-int main (){
+int main(){
     char exp[100];
-    cout<<"Enter the expression:";
-    cin.getline(exp,100);
-for (int i=0;exp[i]!='\0'; i++){
-   if ((exp[i] >= 'A' && exp[i] <= 'Z') || (exp[i] >= 'a' && exp[i] <= 'z')) {
+
+    cout << "Enter the expression: ";
+    cin.getline(exp, 100);
+
+    for(int i = 0; exp[i] != '\0'; i++){
+
+        // Operand (A-Z, a-z)
+        if((exp[i] >= 'A' && exp[i] <= 'Z') || 
+           (exp[i] >= 'a' && exp[i] <= 'z')){
             cout << exp[i];
         }
-       else {
-            while (!isEmpty() && precedence(peek()) >= precedence(exp[i])) {
-                cout << peek();
-                pop();
+
+        // Opening bracket
+        else if(exp[i] == '('){
+            push(exp[i]);
+        }
+
+        // Closing bracket
+        else if(exp[i] == ')'){
+            while(!isEmpty() && peek() != '('){
+                cout << pop();
+            }
+            pop(); // remove '('
+        }
+
+        // Operator
+        else{
+            while(!isEmpty() && precedence(peek()) >= precedence(exp[i])){
+                cout << pop();
             }
             push(exp[i]);
         }
-}
-while (!isEmpty()) {
-        cout << peek();
-        pop();
+    }
+
+    // Pop remaining operators
+    while(!isEmpty()){
+        cout << pop();
     }
 
     return 0;
-
-
 }
