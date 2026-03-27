728fe1c5a4f42e9d37dce793ceda5a1ff297226a
Nida Arshad <nidaarshad120@gmail.com>
2026-03-26 20:32:25 -0700

    Deque using doubly linked list

---
+++ mypractice/Queue/Deque.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int value) {
+        data = value;
+        next = prev = nullptr;
+    }
+};
+
+class Deque {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Deque() {
+        front = rear = nullptr;
+    }
+
+    bool empty() {
+        return (front == nullptr);
+    }
+
+    void push_front(int value) {
+        Node* newnode = new Node(value);
+
+        if (empty()) {
+            front = rear = newnode;
+        } else {
+            newnode->next = front;
+            front->prev = newnode;
+            front = newnode;
+        }
+    }
+
+    void push_back(int value) {
+        Node* newnode = new Node(value);
+
+        if (empty()) {
+            front = rear = newnode;
+        } else {
+            rear->next = newnode;
+            newnode->prev = rear;
+            rear = newnode;
+        }
+    }
+
+    void pop_front() {
+        if (empty()) {
+            cout << "Empty\n";
+            return;
+        }
+
+        Node* temp = front;
+        front = front->next;
+
+        if (front != nullptr)
+            front->prev = nullptr;
+        else
+            rear = nullptr;
+
+        delete temp;
+    }
+
+    void pop_back() {
+        if (empty()) {
+            cout << "Empty\n";
+            return;
+        }
+
+        Node* temp = rear;
+        rear = rear->prev;
+
+        if (rear != nullptr)
+            rear->next = nullptr;
+        else
+            front = nullptr;
+
+        delete temp;
+    }
+
+    int getFront() {
+        if (empty()) return -1;
+        return front->data;
+    }
+
+    int getRear() {
+        if (empty()) return -1;
+        return rear->data;
+    }
+
+    void display() {
+        Node* temp = front;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Deque d;
+
+    d.push_back(10);
+    d.push_back(20);
+    d.push_front(5);
+
+    d.display();
+
+    d.pop_front();
+    d.display();
+
+    d.pop_back();
+    d.display();
+
+    return 0;
+}
