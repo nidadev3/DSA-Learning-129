bb3cb8b93b8b5bf2397ad25cea9bf6a3f36a883b
Nida Arshad <nidaarshad120@gmail.com>
2026-03-26 19:55:58 -0700

    Queue using linked list

---
+++ mypractice/Queue/QueueLinkedList.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* link;
+
+    Node(int value) {
+        data = value;
+        link = nullptr;
+    }
+};
+
+class Queue {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    Queue() {
+        head = tail = nullptr;
+    }
+
+    void push(int value) {
+        Node* newnode = new Node(value);
+        if (empty()) {
+            head = tail = newnode;
+        } else {
+            tail->link = newnode;
+            tail = newnode;
+        }
+    }
+
+    int pop() {
+        if (empty()) {
+            cout << "The Queue is Empty\n";
+            return -1;
+        }
+        Node* temp = head;
+        int value = temp->data;
+        head = head->link;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        delete temp;
+        return value;
+    }
+
+    int front() {
+        if (empty()) {
+            cout << "The Queue is Empty\n";
+            return -1;
+        }
+        return head->data;
+    }
+
+    bool empty() {
+        return (head == nullptr);
+    }
+
+    void display() {
+        if (empty()) {
+            cout << "The Queue is Empty\n";
+            return;
+        }
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->link;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    q.display();
+
+    cout << q.pop() << endl;
+    q.display();
+
+    cout << q.front() << endl;
+
+    return 0;
+}
