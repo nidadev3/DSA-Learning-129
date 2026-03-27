58d0c64e933b9c0e754a19d7ca0bf528dfad20b5
Nida Arshad <nidaarshad120@gmail.com>
2026-03-26 20:01:37 -0700

    Queue using stl

--- mypractice/Queue/QueueLinkedList.cpp
+++ mypractice/Queue/QueueLinkedList.cpp
@@ -1,88 +1,105 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* link;
 
-    Node(int value) {
-        data = value;
-        link = nullptr;
-    }
-};
+// struct Node {
+//     int data;
+//     Node* link;
 
-class Queue {
-private:
-    Node* head;
-    Node* tail;
+//     Node(int value) {
+//         data = value;
+//         link = nullptr;
+//     }
+// };
 
-public:
-    Queue() {
-        head = tail = nullptr;
-    }
+// class Queue {
+// private:
+//     Node* head;
+//     Node* tail;
 
-    void push(int value) {
-        Node* newnode = new Node(value);
-        if (empty()) {
-            head = tail = newnode;
-        } else {
-            tail->link = newnode;
-            tail = newnode;
-        }
-    }
+// public:
+//     Queue() {
+//         head = tail = nullptr;
+//     }
 
-    int pop() {
-        if (empty()) {
-            cout << "The Queue is Empty\n";
-            return -1;
-        }
-        Node* temp = head;
-        int value = temp->data;
-        head = head->link;
-        if (head == nullptr) {
-            tail = nullptr;
-        }
-        delete temp;
-        return value;
-    }
+//     void push(int value) {
+//         Node* newnode = new Node(value);
+//         if (empty()) {
+//             head = tail = newnode;
+//         } else {
+//             tail->link = newnode;
+//             tail = newnode;
+//         }
+//     }
 
-    int front() {
-        if (empty()) {
-            cout << "The Queue is Empty\n";
-            return -1;
-        }
-        return head->data;
-    }
+//     int pop() {
+//         if (empty()) {
+//             cout << "The Queue is Empty\n";
+//             return -1;
+//         }
+//         Node* temp = head;
+//         int value = temp->data;
+//         head = head->link;
+//         if (head == nullptr) {
+//             tail = nullptr;
+//         }
+//         delete temp;
+//         return value;
+//     }
 
-    bool empty() {
-        return (head == nullptr);
-    }
+//     int front() {
+//         if (empty()) {
+//             cout << "The Queue is Empty\n";
+//             return -1;
+//         }
+//         return head->data;
+//     }
 
-    void display() {
-        if (empty()) {
-            cout << "The Queue is Empty\n";
-            return;
-        }
-        Node* temp = head;
-        while (temp != nullptr) {
-            cout << temp->data << " -> ";
-            temp = temp->link;
-        }
-        cout << "NULL\n";
-    }
-};
+//     bool empty() {
+//         return (head == nullptr);
+//     }
 
-int main() {
-    Queue q;
+//     void display() {
+//         if (empty()) {
+//             cout << "The Queue is Empty\n";
+//             return;
+//         }
+//         Node* temp = head;
+//         while (temp != nullptr) {
+//             cout << temp->data << " -> ";
+//             temp = temp->link;
+//         }
+//         cout << "NULL\n";
+//     }
+// };
 
+// int main() {
+//     Queue q;
+
+//     q.push(10);
+//     q.push(20);
+//     q.push(30);
+
+//     q.display();
+
+//     cout << q.pop() << endl;
+//     q.display();
+
+//     cout << q.front() << endl;
+
+//     return 0;
+// }
+
+//using stl
+int main (){
+    queue<int>q;
     q.push(10);
     q.push(20);
     q.push(30);
+      cout << q.front() << endl;
 
-    q.display();
-
-    cout << q.pop() << endl;
-    q.display();
+    q.pop();
 
     cout << q.front() << endl;
 
