0681df49c2c8a41d00a87eeb84e41a943b0c0de8
Nida Arshad <nidaarshad120@gmail.com>
2026-03-31 07:30:48 -0700

    Basic queue operations

---
+++ Assignments/Queue/FirstNonRepeatingCharacter.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class queue{private:
+int *arr;
+int capacity;
+int size;
+int front;
+int rear;
+public:
+queue(int cap){
+    capacity=cap;
+    arr=new int[capacity];
+    size=0;
+    front=0;
+rear=-1;
+}
+int getCapacity() {
+    return capacity;
+}
+bool rempty(){
+    return size==0;
+
+}
+bool rfull(){
+    return size==capacity;}
+    void enqueue(int value){
+        if(rfull()){
+            cout<<"Queue is full\n";
+            return;
+
+        }
+        rear=(rear+1)%capacity;
+        arr[rear]=value;    
+        size++;
+
+    }
+int dequeue(){
+    if(rempty()){
+        cout<<"Queue is empty\n";
+        return -1;
+    }
+    int value=arr[front];
+    front=(front+1)%capacity;
+    size--;
+    return value;
+}};
