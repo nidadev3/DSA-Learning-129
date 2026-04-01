75f01507cc4dcf5319b08429f106500de1d59569
Nida Arshad <nidaarshad120@gmail.com>
2026-03-31 23:39:43 -0700

    Added basic functions of queue

---
+++ Assignments/Queue/QueueusingStack.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class MyQueue{
+    public:
+    MyQueue(){
+
+    }
+    void push(int x){
+
+    }
+    int pop(){
+
+    }
+    int peek(){
+
+    }
+    bool empty(){
+        
+    }
+}
