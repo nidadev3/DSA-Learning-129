1088225c471fcf7e0663f9a55040c4a97a7b2fb5
Nida Arshad <nidaarshad120@gmail.com>
2026-03-31 22:20:29 -0700

    Queue operations according to the printer scenerion

--- Assignments/Queue/PrintQueue.cpp
+++ Assignments/Queue/PrintQueue.cpp
@@ -3,5 +3,45 @@ using namespace std;
 struct job{
     int id;
     string ownerName;
-    stirng documentName;
+    string documentName;
 };
+class queue {
+private:
+  job *arr;
+    int capacity;
+    int size;
+    int front;
+    int rear;
+
+public:
+    queue(int cap) {
+        capacity = cap;
+        arr = new job[capacity];
+        size = 0;
+        front = 0;
+        rear = -1;
+    }
+
+    bool rempty() {
+        return size == 0;
+    }
+
+    bool rfull() {
+        return size == capacity;
+    }
+
+    void enqueue(job b) {
+        if (rfull()) return;
+        rear = (rear + 1) % capacity;
+        arr[rear] = b;
+        size++;
+    }
+
+ job dequeue() {
+    if(rempty()) return job(); 
+    job b = arr[front];        
+    front = (front+1) % capacity;
+    size--;
+    return b;                   
+}
+}
