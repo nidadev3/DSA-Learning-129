48053351fae50b34d8fca0e7358774ccbc5d795a
Nida Arshad <nidaarshad120@gmail.com>
2026-03-31 23:20:47 -0700

    Basic struccture of stack

---
+++ Assignments/Queue/StackusingQueue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class MyStack{
+    public:
+    MyStack(){
+
+    }
+    void push(int x){
+
+    }
+    int pop(){
+
+    }
+    int top(){
+
+    }
+    bool empty(){
+
+    }
+};
