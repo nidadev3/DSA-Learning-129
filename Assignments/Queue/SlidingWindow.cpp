8c8da2115f5557e19795c21a74fd170d47667d65
Nida Arshad <nidaarshad120@gmail.com>
2026-03-31 21:34:15 -0700

    Added main function for sliding window program

--- Assignments/Queue/SlidingWindow.cpp
+++ Assignments/Queue/SlidingWindow.cpp
@@ -1,47 +1,86 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class queue{
-    private:
+
+class Queue {
+private:
     int* arr;
     int size;
     int front;
     int rear;
-    public:
-    queue(int n){
-  arr=new arr[n];
-  size=n;
-  front=0;
-  rear=-1;
-}
-bool isfull(){
-    return rear=size-1;
-}
-bool isempty(){
-    return front>rear;
-}
-void equeue(int value){
-    if(isfull()){
-        cout<<"Queue is overflow.";
+
+public:
+    Queue(int n) {
+        arr = new int[n];
+        size = n;
+        front = 0;
+        rear = -1;
     }
-    rear++;
-    arr[rear]=x;
-}
-void dequeue(){
-    if(isempty()){
-        cout<<"Queue is underflow.";
+
+    bool isFull() {
+        return rear == size - 1;
     }
-front++;
-}
-int findMax(){
-    if(isempty()){
-        cout<<"Queue is underflow.";
+
+    bool isEmpty() {
+        return front > rear;
     }
-    int max=arr[front];
-    for(int i=front;i<=rear;i++){
-        if(arr[i]>max){
-            max=arr[i];
+
+    void enqueue(int value) {
+        if(isFull()) {
+            cout << "Queue Overflow\n";
+            return;
         }
-        return max;
+        rear++;
+        arr[rear] = value;
     }
-}
+
+    void dequeue() {
+        if(isEmpty()) {
+            cout << "Queue Underflow\n";
+            return;
+        }
+        front++;
+    }
+
+    int findMax() {
+        int maxVal = arr[front];
+        for(int i = front; i <= rear; i++) {
+            if(arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
 };
+
+int main() {
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cout << "Enter element " << i+1 << ": ";
+        cin >> arr[i];
+    }
+
+    int k;
+    cout << "Enter window size: ";
+    cin >> k;
+
+    Queue q(k);
+
+    for(int i = 0; i < k; i++) {
+        q.enqueue(arr[i]);
+    }
+
+    cout << q.findMax() << " ";
+
+    for(int i = k; i < n; i++) {
+        q.dequeue();
+        q.enqueue(arr[i]);
+        cout << q.findMax() << " ";
+    }
+
+    return 0;
+}
