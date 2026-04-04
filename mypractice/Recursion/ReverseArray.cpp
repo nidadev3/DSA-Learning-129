3031ce1b4639abdfbcc9517e9965d3633966f879
Nida Arshad <nidaarshad120@gmail.com>
2026-04-04 01:09:43 -0700

    Reverse an array using recusrion

---
+++ mypractice/Recursion/ReverseArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end) {
+   
+    if (start >= end) return;
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+   
+    reverseArray(arr, start + 1, end - 1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    reverseArray(arr, 0, n - 1);
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
