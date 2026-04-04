ccd4e90808e76e7172a75ba76d84bd7f095e7ac7
Nida Arshad <nidaarshad120@gmail.com>
2026-04-03 23:10:29 -0700

    Complete function for printing subset of an array

--- mypractice/Recursion/PrintSubset.cpp
+++ mypractice/Recursion/PrintSubset.cpp
@@ -6,10 +6,17 @@ void psubset(int arr[], int n, int subset[], int i, int subsetsize){
         for(int j=0;j<subsetsize;j++){
             cout<<subset[j]<<" ";
         }
-        cout<<"{" <<endl; 
+        cout<<"}" <<endl; 
         return;
     }
-    subset[subsetsize]==arr[i];
-    psubset(arr,subset,i+1,subsetsize+1);
+    subset[subsetsize]=arr[i];
+    psubset(arr,n,subset,i+1,subsetsize+1);
     psubset(arr,n,subset,i+1,subsetsize);
+}
+int main(){
+    int arr[]={1,2,3,4};
+    int n=4;
+    int subset[4];
+    psubset(arr,n,subset,0,0);
+    return 0;
 }
