848a882ee255f2a0cdbc0d05f643e16a9aec9ae8
Nida Arshad <nidaarshad120@gmail.com>
2026-04-03 23:06:42 -0700

    Print subset function

--- mypractice/Recursion/PrintSubset.cpp
+++ mypractice/Recursion/PrintSubset.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void subset(int arr[], int n, int subset[], int i, int subsetsize){
+void psubset(int arr[], int n, int subset[], int i, int subsetsize){
     if(i==n){
         cout<<"{";
         for(int j=0;j<subsetsize;j++){
@@ -9,5 +9,7 @@ void subset(int arr[], int n, int subset[], int i, int subsetsize){
         cout<<"{" <<endl; 
         return;
     }
-    
+    subset[subsetsize]==arr[i];
+    psubset(arr,subset,i+1,subsetsize+1);
+    psubset(arr,n,subset,i+1,subsetsize);
 }
