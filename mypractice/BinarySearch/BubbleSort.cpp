360032a9c3f2d08e101d563b69074aaee87ef106
Nida Arshad <nidaarshad120@gmail.com>
2026-04-05 04:09:48 -0700

    Bubble sorting by taking user input

--- mypractice/BinarySearch/BubbleSort.cpp
+++ mypractice/BinarySearch/BubbleSort.cpp
@@ -13,7 +13,6 @@ int main() {
         cin >> arr[i];
     }
 
-    // Bubble Sort
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
@@ -30,4 +29,4 @@ int main() {
     }
 
     return 0;
-}
+}  
