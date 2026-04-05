be6b3c2118beea4f09deb2bc897e3f25c197e4df
Nida Arshad <nidaarshad120@gmail.com>
2026-04-05 08:54:11 -0700

    Check balanced brackets using stack

---
+++ Labs/Lab2/StackTask1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string s) {
+    stack<char> st;
+
+    for(int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+
+        if(ch == '(' || ch == '{' || ch == '[') {
+            st.push(ch);
+        }
+        else {
+            if(st.empty()) return false;
+
+            if((ch == ')' && st.top() == '(') ||
+               (ch == '}' && st.top() == '{') ||
+               (ch == ']' && st.top() == '[')) {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+
+    string s;
+    cin >> s;
+
+    if(isBalanced(s))
+        cout << "Balanced";
+    else
+        cout << "Not Balanced";
+
+    return 0;
+}
