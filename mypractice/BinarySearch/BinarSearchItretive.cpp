7656979ed1f26dce751f11d2c028333ab2029d31
Nida Arshad <nidaarshad120@gmail.com>
2026-04-05 12:02:39 -0700

    Binary search through  iterative searching

--- mypractice/BinarySearch/BinarSearchItretive.cpp
+++ mypractice/BinarySearch/BinarSearchItretive.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    //array
+
+int main() {
+    // Array input
     int n;
-    cout<<"Enter the capacity of the array:";
-    cin>>n;
-   int arr[n];
-     cout<<"Enter the elements of the array:\n"
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "Enter the capacity of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    //bubble sort
-      for(int i = 0; i < n - 1; i++) {
+    for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 int temp = arr[j];
@@ -22,4 +24,41 @@ int main (){
         }
     }
 
+   
+    cout << "\nSorted array:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+cout<<endl;
+    
+    int target;
+    cout << "Enter the element you want to search: "<<endl;
+    cin >> target;
+
+    int start = 0;
+    int end = n - 1;
+    int mid;
+    bool found = false;
+
+    while(start <= end) {
+        mid = (start + end) / 2;
+
+        if(arr[mid] == target) {
+            cout << "Element found at index: " << mid << endl;
+            found = true;
+            break;
+        }
+        else if(arr[mid] < target) {
+            start = mid + 1;  
+        }
+        else {
+            end = mid - 1;     
+        }
+    }
+
+    if(!found) {
+        cout << "Element not found in array.\n";
+    }
+
+    return 0;
 }
