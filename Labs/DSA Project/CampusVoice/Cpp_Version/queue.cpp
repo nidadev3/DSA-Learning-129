31c80bc1431920aa8834ad907e784f521d97d566
Nida Arshad <nidaarshad120@gmail.com>
2026-04-28 05:12:52 -0700

    Check if there is any complaint or not

--- Labs/DSA Project/CampusVoice/Cpp_Version/queue.cpp
+++ Labs/DSA Project/CampusVoice/Cpp_Version/queue.cpp
@@ -2,7 +2,14 @@
 #include "Queue.h"
 
 using namespace std;
+//constructor
 Queue::Queue() {
     front = 0;
     rear = -1;
 }
+
+//ifEmpty
+bool Queue::isEmpty() {
+    return (front > rear);
+}
+
