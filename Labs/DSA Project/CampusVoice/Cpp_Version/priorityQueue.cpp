eb9891c2b42d16bd646b45309a858f7195bea31c
Nida Arshad <nidaarshad120@gmail.com>
2026-04-28 07:32:57 -0700

    Added complaint to the priority queue

--- Labs/DSA Project/CampusVoice/Cpp_Version/priorityQueue.cpp
+++ Labs/DSA Project/CampusVoice/Cpp_Version/priorityQueue.cpp
@@ -12,3 +12,25 @@ PriorityQueue::PriorityQueue() {
 bool PriorityQueue::isEmpty() {
     return size == 0;
 }
+
+//Adding important Complaintz
+void PriorityQueue::enqueue(Complaint c) {
+    if (size >= 100) {
+        cout << "Priority Queue Full!" << endl;
+        return;
+    }
+
+    int i;
+    for (i = size - 1; i >= 0; i--) {
+        if (arr[i].isUrgent == false && c.isUrgent == true) {
+            arr[i + 1] = arr[i];
+        } else {
+            break;
+        }
+    }
+
+    arr[i + 1] = c;
+    size++;
+
+    cout << "Complaint added to Priority Queue!" << endl;
+}
