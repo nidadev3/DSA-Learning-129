79fb2f5c35155aaaa06de1b349d85f4f22f02c82
Nida Arshad <nidaarshad120@gmail.com>
2026-04-28 08:47:17 -0700

    Display of resolved complaints

--- Labs/DSA Project/CampusVoice/Cpp_Version/stack.cpp
+++ Labs/DSA Project/CampusVoice/Cpp_Version/stack.cpp
@@ -33,3 +33,20 @@ Complaint Stack::pop() {
 
     return arr[top--];
 }
+
+//Display 
+void Stack::display() {
+    if (isEmpty()) {
+        cout << "No resolved complaints!" << endl;
+        return;
+    }
+
+    for (int i = top; i >= 0; i--) {
+        cout << "ID: " << arr[i].id << endl;
+        cout << "Name: " << arr[i].studentName << endl;
+        cout << "Category: " << arr[i].category << endl;
+        cout << "Urgent: " << (arr[i].isUrgent ? "Yes" : "No") << endl;
+        cout << "Status: " << arr[i].status << endl;
+        cout << "-------------------" << endl;
+    }
+}
