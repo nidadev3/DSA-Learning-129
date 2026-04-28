bde647d6cbde635492e5228a0418017d67d6c384
Nida Arshad <nidaarshad120@gmail.com>
2026-04-28 08:53:34 -0700

    Insertion at the end of the linked liat

--- Labs/DSA Project/CampusVoice/Cpp_Version/linkedList.cpp
+++ Labs/DSA Project/CampusVoice/Cpp_Version/linkedList.cpp
@@ -7,3 +7,24 @@ using namespace std;
 LinkedList::LinkedList() {
     head = NULL;
 }
+
+//insert add at end
+void LinkedList::insert(Complaint c) {
+    Node* newNode = new Node;
+    newNode->data = c;
+    newNode->next = NULL;
+
+    // if empty list
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    // traverse to last node
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
