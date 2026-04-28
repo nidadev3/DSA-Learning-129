53e0a453f6ee654430f7536bbc1a4d4e9314ed62
Nida Arshad <nidaarshad120@gmail.com>
2026-04-28 04:23:16 -0700

    Added complaint structure

---
+++ Labs/DSA Project/CampusVoice/Cpp_Version/complaint.h
@@ -0,0 +1,8 @@
+struct Complaint{
+    int id;
+    string studentName;
+    string description;
+    string category; //lab,hostel,cafe
+    bool isUrgent;
+    string status;   //pending or resolved
+};
