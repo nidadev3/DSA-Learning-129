bcb38f05422ae239f9d3a70a16381fdf2484ac69
Nida Arshad <nidaarshad120@gmail.com>
2026-05-05 11:19:20 -0700

    Added input graph for finding the shortest path between 2 nodes using A* algo

---
+++ Labs/GraphAssignment/ShortestPath.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<int,int>> adj[n+1];
+
+    // input graph
+    for(int i = 0; i < m; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+    
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+}
