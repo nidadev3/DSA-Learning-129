7dcb7e013d1bf5feb926df437857b8109466cd55
Nida Arshad <nidaarshad120@gmail.com>
2026-05-05 17:49:29 -0700

    Main logic for dj algo

--- Labs/GraphAssignment/DijkstraAlgoForSP.cpp
+++ Labs/GraphAssignment/DijkstraAlgoForSP.cpp
@@ -24,4 +24,32 @@ int main() {
 
         adj[u].push_back({v, w});
         adj[v].push_back({u, w}); // undirected graph
-    }}
+    }
+      int source;
+    cout << "Enter source node: ";
+    cin >> source;
+
+    vector<int> dist(n+1, INT_MAX);
+    dist[source] = 0;
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    pq.push({0, source}); // (distance, node)
+
+    while(!pq.empty()){
+
+        int node = pq.top().second;
+        pq.pop();
+
+        for(auto x : adj[node]){
+
+            int v = x.first;
+            int w = x.second;
+
+            if(dist[node] + w < dist[v]){
+
+                dist[v] = dist[node] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
