b72839427a22b51fe7429319bbe69bb3ea0deda3
Nida Arshad <nidaarshad120@gmail.com>
2026-05-07 00:00:58 -0700

    Weighted Graph representation using matrix

--- Assignments/Graphs/WeightedGraph.cpp
+++ Assignments/Graphs/WeightedGraph.cpp
@@ -1,38 +1,31 @@
 //Representation using Matrix
-//Representeation using list
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+
     int n, m;
+    cin >> n >> m;
 
-    cout << "Enter the number of nodes/vertices of the graph: ";
-    cin >> n;
+    int adj[100][100] = {0};
 
-    cout << "Enter the number of edges of the graph: ";
-    cin >> m;
+    
+    for (int i = 0; i < m; i++) {
 
-    // Adjacency list
-    vector<pair<int, int>> adj[n + 1];
-
-    cout << "Enter edges (u v w):\n";
-
-    for(int i = 0; i < m; i++){
         int u, v, w;
+
         cin >> u >> v >> w;
 
-        adj[u].push_back({v, w});
-        adj[v].push_back({u, w}); 
+        
+        adj[u][v] = w;
     }
 
-    cout << "\nGraph (Adjacency List with weights):\n";
+   
+    for (int i = 0; i < n; i++) {
 
-    for(int i = 1; i <= n; i++){
-        cout << i << " -> ";
+        for (int j = 0; j < n; j++) {
 
-        for(int j = 0; j < adj[i].size(); j++){
-            cout << "(" << adj[i][j].first << ", " << adj[i][j].second << ") ";
+            cout << adj[i][j] << " ";
         }
 
         cout << endl;
@@ -40,3 +33,44 @@ int main(){
 
     return 0;
 }
+//Representeation using list
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     int n, m;
+
+//     cout << "Enter the number of nodes/vertices of the graph: ";
+//     cin >> n;
+
+//     cout << "Enter the number of edges of the graph: ";
+//     cin >> m;
+
+//     // Adjacency list
+//     vector<pair<int, int>> adj[n + 1];
+
+//     cout << "Enter edges (u v w):\n";
+
+//     for(int i = 0; i < m; i++){
+//         int u, v, w;
+//         cin >> u >> v >> w;
+
+//         adj[u].push_back({v, w});
+//         adj[v].push_back({u, w}); 
+//     }
+
+//     cout << "\nGraph (Adjacency List with weights):\n";
+
+//     for(int i = 1; i <= n; i++){
+//         cout << i << " -> ";
+
+//         for(int j = 0; j < adj[i].size(); j++){
+//             cout << "(" << adj[i][j].first << ", " << adj[i][j].second << ") ";
+//         }
+
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
