62cfc90c79120de44d848024b5a88865679bf5e3
Nida Arshad <nidaarshad120@gmail.com>
2026-05-08 18:12:12 -0700

    Main function for counting Edges  and vertices of a graph

---
+++ Assignments/Graphs/TotalEdgeandVertex.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+usingg namespace std;
+
+int countVertices(int n){
+
+
+}
+int countEdges(){
+
+    
+}
+int main() {
+
+    int n, m;
+
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    int type;
+
+    cout << "Enter graph type (0 = Undirected, 1 = Directed): ";
+    cin >> type;
+
+    vector<int> adj[n];
+
+    cout << "Enter edges (u v):\n";
+
+    for(int i = 0; i < m; i++) {
+
+        int u, v;
+        cin >> u >> v;
+
+        if(type == 0) {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        else {
+            adj[u].push_back(v);
+        }
+    }
+
+    cout << "\nTotal Vertices = "
+         << countVertices(n) << endl;
+
+    cout << "Total Edges = "
+         << countEdges(adj, n, type) << endl;
+
+    return 0;
+}
