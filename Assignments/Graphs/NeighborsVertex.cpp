ec9212ddfb87c3e30715214d8d4a34155f51724f
Nida Arshad <nidaarshad120@gmail.com>
2026-05-08 18:19:02 -0700

    Main function to find the neigbhors of a vertex

---
+++ Assignments/Graphs/NeighborsVertex.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void displayNeighbors(int node, vector<int> adj[]) {
+
+}
+
+int main() {
+
+    int n, m;
+
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    int type;
+
+    cout << "Enter graph type (0 = Undirected, 1 = Directed): ";
+    cin >> type;
+
+    vector<int> adj[n];
+
+    cout << "Enter edges (u v):\n";
+
+    for(int i = 0; i < m; i++) {
+
+        int u, v;
+        cin >> u >> v;
+
+        if(type == 0) {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        else {
+            adj[u].push_back(v);
+        }
+    }
+
+    // Print adjacency list
+    cout << "\nAdjacency List:\n";
+
+    for(int i = 0; i < n; i++) {
+
+        cout << i << " -> ";
+
+        for(int j = 0; j < adj[i].size(); j++) {
+            cout << adj[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    int node;
+
+    cout << "\nEnter vertex to find neighbors: ";
+    cin >> node;
+
+    displayNeighbors(node, adj);
+
+    return 0;
+}
