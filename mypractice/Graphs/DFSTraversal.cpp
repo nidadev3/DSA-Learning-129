e9abdc0f4405ee733496522d4981b4a041cf4acc
Nida Arshad <nidaarshad120@gmail.com>
2026-05-09 23:46:11 -0700

    Added main function

--- mypractice/Graphs/DFSTraversal.cpp
+++ mypractice/Graphs/DFSTraversal.cpp
@@ -16,3 +16,33 @@ void DFS(vector<int>adj[],vector<bool>&visited,int current){
         }
     }
 }
+int main()
+{
+    int vertices = 5;
+
+    // Adjacency List
+    vector<int> adj[5];
+
+    // Adding edges
+    adj[0].push_back(1);
+    adj[0].push_back(2);
+
+    adj[1].push_back(0);
+    adj[1].push_back(3);
+    adj[1].push_back(4);
+
+    adj[2].push_back(0);
+
+    adj[3].push_back(1);
+
+    adj[4].push_back(1);
+
+    // Visited array
+    vector<bool> visited(vertices, false);
+
+    cout << "DFS Traversal: ";
+
+    DFS(adj, visited, 0);
+
+    return 0;
+}
