f19322a58fd54c08f993a2be2af6331b34e8d182
Nida Arshad <nidaarshad120@gmail.com>
2026-05-15 23:11:27 -0700

    Create new node

--- mypractice/Trees/AVLTraversal.cpp
+++ mypractice/Trees/AVLTraversal.cpp
@@ -9,3 +9,14 @@ struct Node
     Node* right;
     int height;
 };
+Node* createNode(int value)
+{
+    Node* newNode = new Node();
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->height = 1;
+
+    return newNode;
+}
