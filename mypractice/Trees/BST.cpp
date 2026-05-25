bba277a3647313cad439e9a3737f480242f52b06
Nida Arshad <nidaarshad120@gmail.com>
2026-05-25 05:20:29 -0700

    Delete function in BST

--- mypractice/Trees/BST.cpp
+++ mypractice/Trees/BST.cpp
@@ -41,3 +41,59 @@ Node* insert(Node*root, int value){
    }
    retun false;
  }
+
+ //delete
+ Node* minNode(Node* root) {
+    while (root->left != NULL)
+        root = root->left;
+
+    return root;
+}
+
+Node* deleteNode(Node* root, int value) {
+
+    if (root == NULL)
+        return NULL;
+
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    }
+
+    else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    }
+
+    else {
+        // NODE FOUND!
+
+        // Case 1: No children
+        if (root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+
+        // Case 2: One child
+
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Case 3: Two children
+        Node* temp = minNode(root->right);
+
+        root->data = temp->data;
+
+        root->right = deleteNode(root->right,
+                                 temp->data);
+    }
+
+    return root;
+}
