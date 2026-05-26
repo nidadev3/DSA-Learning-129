cd7a74f85a35aa7273bcf278b934ebda5ed3ac26
Nida Arshad <nidaarshad120@gmail.com>
2026-05-25 20:35:26 -0700

    Added main function

--- mypractice/Trees/IsBst.cpp
+++ mypractice/Trees/IsBst.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 struct Node{
@@ -12,15 +13,21 @@ struct Node{
         right=NULL;
     }
 };
+Node* insert(Node* root, int value){
 
-bool isless(Node * root , int value){
+    if(root==NULL){
+        return new Node(value);
+    }
 
+    if(value < root->data){
+        root->left = insert(root->left, value);
+    }
+    else if(value > root->data){
+        root->right = insert(root->right, value);
+    }
+
+    return root;
 }
-
-bool isgreat(Node * root , int value){
-
-}
-
 bool isBSTUtil(Node* root, int minValue, int maxValue){
 
     if(root==NULL){
@@ -39,4 +46,23 @@ bool isBSTUtil(Node* root, int minValue, int maxValue){
 
 bool isBST(Node* root){
     return isBSTUtil(root, INT_MIN, INT_MAX);
+}
+int main(){
+
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    if(isBST(root))
+        cout << "Tree is BST" << endl;
+    else
+        cout << "Tree is NOT BST" << endl;
+
+    return 0;
 }
