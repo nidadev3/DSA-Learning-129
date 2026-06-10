a590b58a90ba222d08cb638fb365c257b1a5ad98
Nida Arshad <nidaarshad120@gmail.com>
2026-06-10 03:29:48 -0700

    Added StudentManager.h

---
+++ Project/SmartUniversity/modules/StudentManager.cpp
@@ -0,0 +1,31 @@
+#ifndef STUDENTMANAGER_H
+#define STUDENTMANAGER_H
+
+#include "../include/DynamicArray.h"
+#include "../include/HashTable.h"
+#include "../include/AVLTree.h"
+#include "../include/BST.h"
+#include "../include/SortingAlgorithms.h"
+#include "../include/SearchAlgorithms.h"
+#include "../include/FileManager.h"
+
+class StudentManager {
+private:
+    DynamicArray dynamicArray;
+    HashTable hashTable;
+    AVLTree avlTree;
+    BST bst;
+    SortingAlgorithms sorter;
+    SearchAlgorithms searcher;
+    FileManager fileManager;
+public:
+    void addStudent(Student s);
+    void deleteStudent(int rollNo);
+    void searchByRollNo(int rollNo);
+    void displayAll();
+    void displaySortedByCGPA();
+    void saveToFile();
+    void loadFromFile();
+};
+
+#endif
