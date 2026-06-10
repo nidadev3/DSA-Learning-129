b7ced5297511ac2a6c1304c891da5ddcb45e9ecd
Nida Arshad <nidaarshad120@gmail.com>
2026-06-10 03:31:36 -0700

    Added StudentManager.cpp

--- Project/SmartUniversity/modules/StudentManager.cpp
+++ Project/SmartUniversity/modules/StudentManager.cpp
@@ -1,31 +1,59 @@
-#ifndef STUDENTMANAGER_H
-#define STUDENTMANAGER_H
+#include "StudentManager.h"
+#include <iostream>
+using namespace std;
 
-#include "../include/DynamicArray.h"
-#include "../include/HashTable.h"
-#include "../include/AVLTree.h"
-#include "../include/BST.h"
-#include "../include/SortingAlgorithms.h"
-#include "../include/SearchAlgorithms.h"
-#include "../include/FileManager.h"
+void StudentManager::addStudent(Student s) {
+    dynamicArray.insert(s);
+    hashTable.insert(s.rollNo, s);
+    avlTree.insert(s);
+    bst.insert(s);
+}
 
-class StudentManager {
-private:
-    DynamicArray dynamicArray;
-    HashTable hashTable;
-    AVLTree avlTree;
-    BST bst;
-    SortingAlgorithms sorter;
-    SearchAlgorithms searcher;
-    FileManager fileManager;
-public:
-    void addStudent(Student s);
-    void deleteStudent(int rollNo);
-    void searchByRollNo(int rollNo);
-    void displayAll();
-    void displaySortedByCGPA();
-    void saveToFile();
-    void loadFromFile();
-};
+void StudentManager::deleteStudent(int rollNo) {
+    hashTable.remove(rollNo);
+    bst.remove(rollNo);
+    cout << "Student " << rollNo << " deleted" << endl;
+}
 
-#endif
+void StudentManager::searchByRollNo(int rollNo) {
+    Student* result = hashTable.search(rollNo);
+    if (result != nullptr) {
+        cout << "Found: " << result->rollNo << " | "
+             << result->name << " | "
+             << result->department << " | "
+             << result->cgpa << endl;
+    } else {
+        cout << "Student not found" << endl;
+    }
+}
+
+void StudentManager::displayAll() {
+    cout << "--- All Students ---" << endl;
+    dynamicArray.display();
+}
+
+void StudentManager::displaySortedByCGPA() {
+    cout << "--- Sorted by CGPA (AVL Inorder) ---" << endl;
+    avlTree.inorder();
+}
+
+void StudentManager::saveToFile() {
+    int size = dynamicArray.getSize();
+    Student* arr = new Student[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = dynamicArray.getAt(i);
+    }
+    fileManager.saveStudents(arr, size, "data/students.txt");
+    delete[] arr;
+    cout << "Saved to file" << endl;
+}
+
+void StudentManager::loadFromFile() {
+    Student arr[1000];
+    int size = 0;
+    fileManager.loadStudents(arr, size, "data/students.txt");
+    for (int i = 0; i < size; i++) {
+        addStudent(arr[i]);
+    }
+    cout << "Loaded " << size << " students" << endl;
+}
