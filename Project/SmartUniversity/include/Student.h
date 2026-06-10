9ad973a277f6a27a8d0037e239ac393d04871490
Nida Arshad <nidaarshad120@gmail.com>
2026-06-09 20:05:30 -0700

    Added students.h

---
+++ Project/SmartUniversity/include/Student.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+using namespace std;
+
+struct Student {
+    int rollNo;
+    string name;
+    string department;
+    float cgpa;
+};
+
+#endif
